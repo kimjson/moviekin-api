f1214afdace91cfb7c4b381b66043e5031d5eeb5
Stein Kim <wotjd1102@gmail.com>
2018-05-23 23:47:02 +0900

    Index action for answer record implemented
    Testing and debugging now.

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::AnswersController < ApplicationController
   def show
     render json: Answer.find(params[:id])
   end
+
+  def index
+    render json: Answer.all
+  end
+
 end
--- spec/api/v1/answers_controller_spec.rb
+++ spec/api/v1/answers_controller_spec.rb
@@ -7,9 +7,25 @@ RSpec.describe Api::V1::AnswersController, type: :request do
       get "/answers/#{@answer.id}"
     end
 
-    it "returns the information about an answer" do
+    it "returns the information about the record" do
       answer_response = JSON.parse(response.body)
       expect(answer_response["content"]).to eql @answer.content
+    end
+
+    it { expect(response).to have_http_status(200) }
+  end
+  describe "GET #index" do
+    before(:each) do
+      @answers = []
+      (1...4).each do |n|
+        @answers << FactoryBot.create :answer
+      end
+      get "/answers"
+    end
+
+    it "returns 4 records from the database" do
+      answers_response = JSON.parse(response.body)
+      expect(answers_response[:answers]).to have(4).items
     end
 
     it { expect(response).to have_http_status(200) }
