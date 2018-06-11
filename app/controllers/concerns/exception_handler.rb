5514a48d5c2b8a225781ca787adfa3068227a69a
Stein Kim <wotjd1102@gmail.com>
2018-06-11 18:24:52 +0900

    Working on error serialization

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,29 +5,36 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
   
-  DEFAULT_TITLE = 'Internal Server Error'
-  DEFAULT_DETAIL = 'It implies problems with backend logic. Contact api develeoper.'
-  
-  def json_error(status=500, title=DEFAULT_TITLE, detail=DEFAULT_DETAIL, source=null)
-    error_hash = Hash.new
-    error_hash[:title] = title
-    error_hash[:detail] = detail
-    if not source.nil? then
-      error_hash[:source] = source
-    end
-    render json: { errors: [error_hash] }, status: status
-  end
-
   included do
     # rescue_from Exceptions::RecordNotFound do |e|
     #   json_error title: e.title, detail: e.detail, status: 404
     # end
     rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: { message: e.message }, status: 404
+      render status: 404, json: {
+        errors: [
+          {
+            status: 404,
+            title: "#{e.model not found}",
+            detail: "#{e.model} not found"
+          }
+        ]
+      }
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: { message: e.message }, status: 422
+      render status: 422, json: {
+        errors: ValidationErrorsSerializer.new(e.record).serialize
+      }
+    end
+
+    rescue_from StandardError do |e|
+      render status: 500, json: {
+        errors: [
+          status: 500,
+          title: 'Unknown error',
+          detail: 'Unknown error. Contact API developer',
+        ]
+      } 
     end
   end
 end
